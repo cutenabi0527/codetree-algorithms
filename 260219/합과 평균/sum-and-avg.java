79e7679e3b78a2654dcc4cf7db72816952140f1d
cutenabi0527 <lgs7042@gmail.com>
2026-02-19 12:13:25 +0900

    Title: 합과 평균, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260219/합과 평균/sum-and-avg.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int c = a+b;
+        double d = c/2.0;
+        System.out.printf("%d"+" "+"%.1f", c,d);
+        	
+    }
+}
