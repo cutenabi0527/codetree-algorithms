fd76fd89e0376631e0e04ac8818fb5d683384c64
cutenabi0527 <lgs7042@gmail.com>
2026-03-03 14:57:28 +0900

    Title: 변수 출력하기, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260303/변수 출력하기/outputing-variables.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    	int a = 7;
+//    	int b = 23;
+//    	int c = 30;
+    	String b = "C";
+//    	System.out.println(a+" + "+b+" = "+c);
+    	System.out.println(a+"..."+b);
+
+
+    }
+}
