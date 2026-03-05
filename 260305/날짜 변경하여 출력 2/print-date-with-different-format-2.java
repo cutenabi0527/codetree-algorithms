ec8e43a9beff37c3657b6601130f9b184692265f
cutenabi0527 <lgs7042@gmail.com>
2026-03-05 17:19:19 +0900

    Title: 날짜 변경하여 출력 2, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        String[] arr = a.split("-");
+
+        System.out.println(arr[2]+"."+arr[0]+"."+arr[1]);
+
+    }
+}
