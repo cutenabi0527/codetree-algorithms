72ac03bc3c3e5237161997bff65db414057b63cf
cutenabi0527 <lgs7042@gmail.com>
2026-03-05 16:54:33 +0900

    Title: 변수 값 복사하기, Time: 85ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260305/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =1;
+        int b = 2;
+        int c = 3;
+        a=b=c;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
