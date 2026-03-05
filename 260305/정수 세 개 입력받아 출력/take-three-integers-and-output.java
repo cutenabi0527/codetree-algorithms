15c2b44fce1b61667934fb769ed1954f14bb38f8
cutenabi0527 <lgs7042@gmail.com>
2026-03-05 17:09:51 +0900

    Title: 정수 세 개 입력받아 출력, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int c =sc.nextInt();
+//    	  Double a =sc.nextDouble();
+//    	  Double b =sc.nextDouble();
+//    	  Double c =sc.nextDouble();
+        System.out.println(a+" "+b+" "+c);
+//        System.out.printf("%.3f\n", a);
+//        System.out.println(a*b);
+    }
+}
