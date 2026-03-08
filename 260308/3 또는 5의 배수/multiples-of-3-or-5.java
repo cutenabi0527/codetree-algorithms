f3fe592ceb75f8af701c99729e4af049cf08552a
cutenabi0527 <lgs7042@gmail.com>
2026-03-08 23:29:02 +0900

    Title: 3 또는 5의 배수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260308/3 또는 5의 배수/multiples-of-3-or-5.java
+++ 260308/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -5,7 +5,7 @@ public class Main {
         int a = sc.nextInt();
 
         if (a%3==0) System.out.println("YES");
-        else System.out.print("NO");
+        else System.out.println("NO");
         if (a%5==0) System.out.println("YES");
         else System.out.println("NO");
 
