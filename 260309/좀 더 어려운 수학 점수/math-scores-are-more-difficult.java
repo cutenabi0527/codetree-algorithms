c93c8272adc1a55171ebdf2b9059ea4e76c0467e
cutenabi0527 <lgs7042@gmail.com>
2026-03-09 13:11:00 +0900

    Title: 좀 더 어려운 수학 점수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260309/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int aM = sc.nextInt();
+		int aE = sc.nextInt();
+		int bM = sc.nextInt();
+		int bE = sc.nextInt();
+		
+
+		if (aM > bM) {
+			System.out.println("A");
+		}
+
+		else if (aM == bM) {
+			if (aE > bE) {
+				System.out.println("A");
+			} else
+				System.out.println("B");
+		}
+		else System.out.println("B");
+	}
+}
