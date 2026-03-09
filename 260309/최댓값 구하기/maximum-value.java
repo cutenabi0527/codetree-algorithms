0d660d0221e0e00610030261b40c10954abf75fc
cutenabi0527 <lgs7042@gmail.com>
2026-03-09 15:22:49 +0900

    Title: 최댓값 구하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 260309/최댓값 구하기/maximum-value.java
+++ 260309/최댓값 구하기/maximum-value.java
@@ -7,18 +7,16 @@ public class Main {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
-		if (a>=b) {
-			if (c>=a) System.out.println(c);
-			else if (b>=c) System.out.println(a);
+		if ((a>=b&&b>=c)||((a>=c&&c>=b))) {
+			System.out.println(a);
 		}
-		else if (b>=a) {
-			if (c>=b) System.out.println(c);
-			else if (a>=c) System.out.println(b);
+		else if ((b>=a&&a>=c)||((b>=c&&c>=a))) {
+			System.out.println(b);
 		}
-		else if (a>=c) {
-			if (c>=b) System.out.println(a);
-			else if (b>=a) System.out.println(b);
+		else if ((c>=a&&a>=b)||((c>=b&&b>=a))) {
+			System.out.println(c);
 		}
+		
 		
 	}
 }
