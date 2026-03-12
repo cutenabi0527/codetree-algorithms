4340cc8be76f5805ab40ad907d698ee195f722c9
cutenabi0527 <lgs7042@gmail.com>
2026-03-12 10:00:03 +0900

    Title: 1까지 나누기, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260312/1까지 나누기/divide-by-1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		int i=1;
+		while(true) {
+			n/=i;
+			cnt++;
+			i++;
+			if (n<=1) break;
+		}
+		System.out.println(cnt);
+	}
+}
+// char a =sc.next().charAt(0);
