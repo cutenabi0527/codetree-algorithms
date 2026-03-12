6978a4cf74de38208133f9dd5f318afc8a4cdad3
cutenabi0527 <lgs7042@gmail.com>
2026-03-12 17:37:27 +0900

    Title: 3N + 1 수열, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 260312/3N + 1 수열/3n-plus-1-sequence.java
+++ 260312/3N + 1 수열/3n-plus-1-sequence.java
@@ -6,6 +6,10 @@ public class Main {
 		int n = sc.nextInt();
 		int cnt=0;
 		while (true) {
+			if (n==1) {
+				
+				break;
+			}
 			if (n%2==0) {
 				n/=2;
 				cnt++;
@@ -13,11 +17,8 @@ public class Main {
 				n=n*3+1;
 				cnt++;
 			}
-			if (n==1) {
-				System.out.println(cnt);
-				break;
-			}
-		}
+			
+		}System.out.println(cnt);
 		
 	}
 }
