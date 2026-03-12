b6ffa1826a7c3b173d8ca9450fee8e8d1312f2fa
cutenabi0527 <lgs7042@gmail.com>
2026-03-12 17:26:45 +0900

    Title: 20대의 평균 나이, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260312/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int cnt=0;
+		while (true) {
+			int a = sc.nextInt();
+			if (a>=20 && a<30) {
+				sum+=a;
+				cnt++;
+			}else {
+				System.out.println((double)sum/cnt);
+				break;
+			}
+		}
+		
+	}
+}
+// char a =sc.next().charAt(0);
