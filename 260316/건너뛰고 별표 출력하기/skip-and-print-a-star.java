a6604ea16348b2dd87e4af87c5b7ec964190ca95
cutenabi0527 <lgs7042@gmail.com>
2026-03-16 14:02:07 +0900

    Title: 건너뛰고 별표 출력하기, Time: 143ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260316/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars;
+            
+            if (i <= n) {
+                stars = i;
+            } else {
+                stars = 2*n-i;
+            }
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
