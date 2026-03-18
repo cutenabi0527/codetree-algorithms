66bd62f8f75b495fd8a6322def68560e3c99e2bc
cutenabi0527 <lgs7042@gmail.com>
2026-03-18 15:24:52 +0900

    Title: hashmap 기본, Time: 1180ms, Memory: 34MB, Status: Passed - Codetree

--- 260318/hashmap 기본/hashmap-basic.java
+++ 260318/hashmap 기본/hashmap-basic.java
@@ -1,32 +1,35 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-public class Main {
+import java.util.HashMap;
+
+public class Main {    
+    // 변수 선언
+    public static int n;
+    public static HashMap<Integer, Integer> m = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int key, value = 0;
-        Map <Integer, Integer> map =new HashMap<>();
-        for (int i=0; i<n; i++) {
-        	String order = sc.next();
-        	if (order.equals("add")) {
-        		key = sc.nextInt();
-        		value = sc.nextInt();
-        	} else {
-        		key = sc.nextInt();
-        	}
-        	
-        	if (order.equals("add")) {
-        		map.put(key, value);
-        	}else if (order.equals("remove")) {
-        		map.remove(key);
-        	}else if (order.equals("find")) {
-        		if (map.containsKey(key)) System.out.println(map.get(key));
-        		else System.out.println("None");
-        	}
-        	
+        // 입력:
+        n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            String command = sc.next();
+
+            if(command.equals("add")) {
+                int k = sc.nextInt();
+                int v = sc.nextInt();
+                m.put(k, v);
+            }
+            else if(command.equals("remove")) {
+                int k = sc.nextInt();
+                m.remove(k);
+            }
+            else {
+                int k = sc.nextInt();
+                if(!m.containsKey(k))
+                    System.out.println("None");
+                else
+                    System.out.println(m.get(k));
+            }
         }
-        
-        
     }
 }
