3ffcd61b36f64db8ccad3e645cd250e598e4a493
cutenabi0527 <lgs7042@gmail.com>
2026-03-19 09:04:33 +0900

    Title: 수 등장 횟수, Time: 460ms, Memory: 24MB, Status: Passed - Codetree

--- 260319/수 등장 횟수/number-frequency.java
+++ 260319/수 등장 횟수/number-frequency.java
@@ -1,45 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.HashMap;
 
-public class Main {    
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static int n, m;
-    public static int[] arr = new int[MAX_N];
-    public static HashMap<Integer, Integer> freq = new HashMap<>();
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        m = sc.nextInt();
-
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Scanner 대신 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        // 각 숫자가 몇 번씩 나왔는지를
-        // hashmap에 기록해줍니다.
-        for(int i = 0; i < n; i++) {
-            // 처음 나온 숫자라면 1을 직접 적어줘야 합니다.
-            if(!freq.containsKey(arr[i]))
-                freq.put(arr[i], 1);
-            // 이미 나와있던 숫자라면 1을 더해줍니다.
-            else
-                freq.put(arr[i], freq.get(arr[i]) + 1);
+        // 첫 번째 줄 입력 (N, M)
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        // 두 번째 줄 입력 (수열 데이터)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        // m개의 질의에 대해
-        // 몇 번씩 나왔는지를 출력합니다.
-        for(int i = 0; i < m; i++) {
-            int num = sc.nextInt();
-
-            // 처음 나온 숫자라면 0을 출력합니다.
-            if(!freq.containsKey(num))
-                System.out.print(0 + " ");
-            // 나온 적이 있는 숫자라면, 빈도수를 출력해줍니다.
-            else
-                System.out.print(freq.get(num) + " ");
+        
+        // 세 번째 줄 입력 (찾고자 하는 수) 및 출력 준비
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder(); // System.out.print 대신 StringBuilder 사용
+        
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(num, 0)).append(" ");
         }
+        
+        // 결과 한 번에 출력
+        System.out.println(sb.toString().trim());
     }
 }
