c437d6d206445a404bb3dde6d1721a4bb6dd9628
cutenabi0527 <lgs7042@gmail.com>
2026-03-20 13:32:49 +0900

    Title: 별표 출력하기 10, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260320/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				for (int j = 0; j < i / 2 + 1; j++) {
+					System.out.print("* ");
+				}
+			} else {
+				for (int j = 0; j < n -(i/2-1); j++) {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+		if (n%2==0){
+			for (int i = 1; i <= n; i++) {
+				if (i % 2 != 0) {
+					for (int j = 0; j < (n+i)/2+1; j++) {
+						System.out.print("* ");
+					}
+				} else {
+					for (int j = 0; j < (n-i)/2+1; j++) {
+						System.out.print("* ");
+					}
+				}
+				System.out.println();
+			}
+		}
+		else {
+			for (int i = 1; i <= n; i++) {
+				if (i % 2 != 0) {
+					for (int j = 0; j < (n-i)/2+1; j++) {
+						System.out.print("* ");
+					}
+				} else {
+					for (int j = 0; j < (n+i)/2+1; j++) {
+						System.out.print("* ");
+					}
+				}
+				System.out.println();
+			}
+
+		}
+		
+	}
+}
