584de3e312a3011666eda1edafafca2e8f577e4c
cutenabi0527 <lgs7042@gmail.com>
2026-03-26 14:26:49 +0900

    Title: 좌우 지그재그 출력, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260326/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int num = 1;
+		int[][] map = new int[a][a];
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < a; j++) {
+				int col = i % 2 == 0 ? j : a - 1 - j;
+				map[i][col] = num++;
+			}
+		}
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < a; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
