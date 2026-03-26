76532c1a032d3bf95e644c6d4dd42f6e0780e9c3
cutenabi0527 <lgs7042@gmail.com>
2026-03-26 11:59:47 +0900

    Title: 행과 열, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 260326/행과 열/row-column.java
+++ 260326/행과 열/row-column.java
@@ -5,11 +5,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int num = 1;
-		for (int i = 0; i < a; i++) {
-			for (int j = 0; j < b; j++) {
-				System.out.print(num + " ");
-				num++;
+
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= b; j++) {
+				System.out.print(i * j + " ");
 			}
 			System.out.println();
 		}
