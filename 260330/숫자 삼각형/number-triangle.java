396a6f1372c60e7365aed6ab496032120afb9504
cutenabi0527 <lgs7042@gmail.com>
2026-03-30 11:59:57 +0900

    Title: 숫자 삼각형, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/숫자 삼각형/number-triangle.java
+++ 260330/숫자 삼각형/number-triangle.java
@@ -5,7 +5,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= a; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(j+" ");
 			}System.out.println();
