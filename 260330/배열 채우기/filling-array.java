8e668dc3ae1671735973ee333204840b90879878
cutenabi0527 <lgs7042@gmail.com>
2026-03-30 17:35:24 +0900

    Title: 배열 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/배열 채우기/filling-array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+		int [] arr = new int [10];
+		
+		for (int i=0; i<10; i++) {
+			int num = sc.nextInt();
+			if (num==0) break;
+			arr[i]=num;
+			
+		}
+		for (int i=9; i>=0; i--) {
+			if (arr[i]!=0) System.out.print(arr[i]+" ");
+
+		} 
+		
+	}
+}
