27c1bf31d5933e2cb03234d4cfd496c851207d76
cutenabi0527 <lgs7042@gmail.com>
2026-03-30 13:09:57 +0900

    Title: 19단 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/19단 출력/nineteen-times-table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+
+		for (int i=1; i<=19; i++) {
+			for (int j=1; j<=19; j++) {
+				System.out.printf("%d * %d = %d", i,j, i*j);
+				if ((j%2!=0)&&j!=19) System.out.print(" / ");
+				else System.out.println();
+			}
+		}
+	}
+}
