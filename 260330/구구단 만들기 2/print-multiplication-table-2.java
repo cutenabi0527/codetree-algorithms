6eb9ebbb537743b13a46809feec940471835c84e
cutenabi0527 <lgs7042@gmail.com>
2026-03-30 11:56:35 +0900

    Title: 구구단 만들기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        for (int j=2; j<=8; j+=2) {
+        	for (int i=b; i>=a; i--) {
+        		System.out.printf("%d * %d = %d", i, j, i*j);
+        		if (i>a) System.out.print(" / ");
+        	} System.out.println();
+        }
+    }
+}
