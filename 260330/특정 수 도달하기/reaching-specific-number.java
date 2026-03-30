5a4eac5824494d19a1da1d5803617576c703cb95
cutenabi0527 <lgs7042@gmail.com>
2026-03-30 17:00:31 +0900

    Title: 특정 수 도달하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+		int [] arr = new int [10];
+		
+		int sum=0;
+		float avg=0;
+		for (int i=0; i<10; i++) {
+			int num = sc.nextInt();
+			if (num>=250) {
+				for (int j=0; j<i; j++) {
+					sum+=arr[j];
+				}
+				avg =  (float)sum/i;
+				break;
+			}
+			arr[i]= num;
+			
+		}
+		System.out.printf("%d %.1f", sum, avg);
+
+	}
+}
