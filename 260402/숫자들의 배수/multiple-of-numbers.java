85c4246604c94c82a63d9f90022640ee3b84e69b
cutenabi0527 <lgs7042@gmail.com>
2026-04-02 17:08:11 +0900

    Title: 숫자들의 배수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		int num =1;
+		while(cnt<2) {
+			if (n*num%5==0) cnt++;
+			System.out.print(n*num+" ");
+			num++;
+			
+		}
+		
+		
+	}
+}
