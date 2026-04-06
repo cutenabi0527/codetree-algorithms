f5a44e4d6c75b706cc3b24d0d613bc319ea8d41e
cutenabi0527 <lgs7042@gmail.com>
2026-04-06 17:42:39 +0900

    Title: 연속부분수열일까, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/연속부분수열일까/contiguous-array-or-not.java
+++ 260406/연속부분수열일까/contiguous-array-or-not.java
@@ -11,40 +11,24 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int N1 = Integer.parseInt(st.nextToken());
 		int N2 = Integer.parseInt(st.nextToken());
-		int[] arr1 = new int[N1];
-		int[] arr2 = new int[N2];
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N1; i++) {
-			arr1[i] = Integer.parseInt(st.nextToken());
+		
+		String arr1 = br.readLine().replace(" ", "");
+		String arr2 = br.readLine().replace(" ", "");
+		
+		if (arr1.length()<arr2.length()){
+			String temp = arr1;
+			arr1 = arr2;
+			arr2 = temp;
 		}
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N2; i++) {
-			arr2[i] = Integer.parseInt(st.nextToken());
-		}
-		String ans = "Yes";
-		int idx1=-1;
-		int idx2=0;
-		for (int i = 0; i < N1; i++) {
-			if (arr1[i]==arr2[0]) {
-				idx1 = i;
+		boolean find = false;
+		for (int i=0; i<arr1.length()-arr2.length()+1;i++) {
+			if (arr2.equals(arr1.substring(i, i+arr2.length()))) {
+				find = true;
 				break;
 			}
 		}
-		if (idx1==-1) ans = "No";
-		while (idx2<N2 && idx1>=0) {
-			if (arr1[idx1]==arr2[idx2]) {
-				idx1++;
-				idx2++;
-			}
-			else {
-				ans = "No";
-				break;
-			}
-		}
-		System.out.println(ans);
-
+		System.out.println(find ? "Yes" : "No");
+		
 		br.close();
 	}
 }
