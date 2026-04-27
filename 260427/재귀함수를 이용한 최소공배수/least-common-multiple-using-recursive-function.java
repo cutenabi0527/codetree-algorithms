63003ed7e2261aa7f819d59a7d3b464369155073
cutenabi0527 <lgs7042@gmail.com>
2026-04-27 17:25:45 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 260427/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -21,24 +21,17 @@ public class Main {
 
 		System.out.println(a(0));
 	}
-	// idx번 원소부터 마지막 원소까지의 최소공배수를 반환하는 함수
 	private static int a(int idx) {
-		// 마지막 원소 하나만 남았다면
-        // 그 구간의 최소공배수는 자기 자신
 		if (idx == n-1) {
 			return arr[idx];
 		}
-		// 현재 값 arr[idx]와
-        // 뒤쪽 구간의 최소공배수 findLcm(idx + 1)의 최소공배수를 구하기
 		return lcm(arr[idx], a(idx+1));
 
 	}
-	// 최대공약수(유클리드 호제법). 최소공배수를 구하기 위해 필요
 	private static int gcd(int a, int b) {
 		if (b ==0) return a;
 		return gcd(b, a%b);
 	}
-	// 최소공배수
 	private static int lcm(int a, int b) {
 		return a/gcd(a, b)*b;
 	}
