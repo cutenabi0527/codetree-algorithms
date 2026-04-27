47d7d5dca0f20b2390bff27247307ee3c913314d
cutenabi0527 <lgs7042@gmail.com>
2026-04-27 13:38:36 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		System.out.println(findMax(0));
+		
+	}
+
+	private static int findMax(int idx) {
+		if (idx==n-1) {
+			return arr[idx];
+		}
+		
+		return Math.max(findMax(idx+1), arr[idx]);
+		
+	}
+
+}
