4ed9c36ccaa1ee6220ae5cb2deb3d400afb62c6c
cutenabi0527 <lgs7042@gmail.com>
2026-04-28 13:59:53 +0900

    Title: 두 개의 동일한 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260428/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		
+		int[] arr1 = new int [n];
+		int[] arr2 = new int [n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<n; i++) {
+			arr1[i]=Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<n; i++) {
+			arr2[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		boolean flag = false;
+		int cnt=0;
+		for (int i=0; i<n; i++) {
+			int a = arr1[i];
+			int b = arr2[i];
+			if (a==b) {
+				cnt++;
+			}
+		}
+		if (cnt==n) flag = true;
+		
+		System.out.println(flag? "Yes" : "No");
+
+		
+	}
+
+
+
+}
