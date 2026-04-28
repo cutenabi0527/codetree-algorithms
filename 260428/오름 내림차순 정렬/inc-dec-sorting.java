3019e8415f2e3520e9c60774ab2d86a4c2c2ea90
cutenabi0527 <lgs7042@gmail.com>
2026-04-28 13:35:21 +0900

    Title: 오름 내림차순 정렬, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260428/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int arr[] = new int [n];
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<n; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int tmp : arr) {
+			System.out.print(tmp+" ");
+		} System.out.println();
+		
+		Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+		Arrays.sort(arr2, Collections.reverseOrder());
+		
+		for (int tmp : arr2) {
+			System.out.print(tmp+" ");
+		}
+		
+		
+	}
+
+
+
+}
