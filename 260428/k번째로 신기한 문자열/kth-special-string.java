658aed9877fe6176efbbda6ec2fff379a6056977
cutenabi0527 <lgs7042@gmail.com>
2026-04-28 15:00:33 +0900

    Title: k번째로 신기한 문자열, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260428/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		String t = st.nextToken();
+		
+		List<String> dic = new ArrayList<>();
+		for (int i=0; i<n; i++) {
+			String tmp = br.readLine();
+			if (tmp.substring(0, t.length()).equals(t)) {
+				dic.add(tmp);
+			}
+		}
+		Collections.sort(dic);
+		System.out.println(dic.get(k+1));
+		
+		
+		
+	}
+
+
+}
