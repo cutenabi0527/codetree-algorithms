03881555c261ff225137a6eeccd7c68076b3549f
cutenabi0527 <lgs7042@gmail.com>
2026-04-30 14:56:51 +0900

    Title: 상품 코드, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/상품 코드/product-code.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Product {
+		String pn;
+		int pc;
+		
+		public Product() {
+			this.pn = "codetree";
+			this.pc = 10;
+		}
+		
+		public Product(String pn, int pc) {
+			this.pn = pn;
+			this.pc = pc;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		Product pd = new Product();
+		
+		System.out.println("product "+pd.pn+" is "+pd.pc);
+		
+		Product pd2 = new Product(st.nextToken(), Integer.parseInt(st.nextToken()));
+		
+		System.out.println("product "+pd2.pn+" is "+pd2.pc);
+	}
+
+}
