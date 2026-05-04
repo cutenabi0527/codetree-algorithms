a2ee0e648a9e19edf26faed9b830d0703359337e
cutenabi0527 <lgs7042@gmail.com>
2026-05-04 11:49:29 +0900

    [Passed] 원점으로부터의 거리 (277ms, 14MB)

--- 260504/원점으로부터의 거리/distance-from-origin.java
+++ 260504/원점으로부터의 거리/distance-from-origin.java
@@ -6,40 +6,42 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Point {
-		int x, y, idx;
 
-		public Point(int x, int y, int idx) {
-			this.x = x;
-			this.y = y;
-			this.idx = idx;
-		}
-		
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(st.nextToken());
-		
-		Point[] pt = new Point[n];
-		
-		for (int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			pt[i]=new Point(Integer.parseInt(st.nextToken()), 
-							Integer.parseInt(st.nextToken()),
-							i+1);
-		}
-		
-		Arrays.sort(pt, Comparator.comparingInt((Point p)-> (Math.abs(p.x)+Math.abs(p.y))));
-		
-		for (Point p : pt) {
-			sb.append(p.idx).append("\n");
-		}
-		
-	
-		System.out.println(sb);
-	}
+    static class Point {
+        int x, y, idx, dist;
 
+        public Point(int x, int y, int idx) {
+            this.x = x;
+            this.y = y;
+            this.idx = idx;
+            this.dist = Math.abs(x) + Math.abs(y);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Point[] pt = new Point[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            pt[i] = new Point(x, y, i + 1);
+        }
+
+        Arrays.sort(pt, Comparator.comparingInt((Point p) -> p.dist));
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Point p : pt) {
+            sb.append(p.idx).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
