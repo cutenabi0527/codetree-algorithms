d31ae932cf7aa4d88554646e2bd0c2cf20b317b5
cutenabi0527 <lgs7042@gmail.com>
2026-05-20 15:38:39 +0900

    [Passed] 데이터 비교 (933ms, 28MB)

---
+++ 260520/데이터 비교/data-comparison.java
@@ -0,0 +1,29 @@
+import java.awt.Image;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        Set<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            System.out.print(set.contains(tmp) ? 1 + " " : 0 + " ");
+        }
+    }
+}
